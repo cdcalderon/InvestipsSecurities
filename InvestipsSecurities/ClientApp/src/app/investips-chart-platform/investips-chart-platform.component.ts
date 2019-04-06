66132faf502168353c7185b2e150b916593f2925
Carlos Calderon <cdcalderon@gmail.com>
2019-04-06 05:19:57 -0500

    fix styling issue

--- InvestipsSecurities/ClientApp/src/app/investips-chart-platform/investips-chart-platform.component.ts
+++ InvestipsSecurities/ClientApp/src/app/investips-chart-platform/investips-chart-platform.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-    selector: 'app-investips-chart-platform',
-    templateUrl: './investips-chart-platform.component.html',
-    styleUrls: ['./investips-chart-platform.component.css']
+    selector: "app-investips-chart-platform",
+    templateUrl: "./investips-chart-platform.component.html",
+    styleUrls: ["./investips-chart-platform.component.css"]
 })
 export class InvestipsChartPlatformComponent implements OnInit {
     marksType: string;
     stockSymbol: string;
     selectedNavSymbol: string;
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute) {}
 
     ngOnInit() {
-        this.selectedNavSymbol = this.route.snapshot.params['id'];
-        this.marksType = this.route.snapshot.params['marktype'];
+        this.selectedNavSymbol = this.route.snapshot.params["id"];
+        this.marksType = this.route.snapshot.params["marktype"];
     }
-
 }
